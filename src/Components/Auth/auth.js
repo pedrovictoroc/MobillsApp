02482ad2bc4527f5f6f5cf4bbcf8be27e6c4e8ab
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-05-15 00:20:09 -0300

    Adicionando Styled-Components e primeira tela de app

---
+++ src/Components/Auth/auth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { View } from 'react-native'
+
+import { Teste } from './styles'
+
+export default function Auth(){
+    return (
+        <View>
+            <Teste> It Works! </Teste>
+        </View>
+    )
+}
---
+++ src/Components/Auth/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const Teste = styled.Text`
+    margin-top: 200px;
+    color: #333;
+    font-weight: bold;
+`
