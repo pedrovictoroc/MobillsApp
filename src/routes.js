14d4431c5f30b27a42c5c0235a4ec0a6eb820cf4
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-05-15 22:02:30 -0300

    Adicionando Router

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
 
-const Routes = createAppContainer(
-    createStackNavigator({
-        Login
-    },{
-        defaultNavigationOptions:{
-            headerShown: false
-        }
-    })
-)
+const AppStack = createStackNavigator()
 
-export default Routes
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator
+                screenOptions={{
+                    headerShown: false   
+                }}
+            >
+                <AppStack.Screen name="Login" component={Login}/>
+                <AppStack.Screen name="SignUp" component={SignUp}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
